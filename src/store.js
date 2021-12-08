1a5d75d186ba8e8401e03709aa893c43663761b0
maze02 <mariasb2@gmail.com>
2021-12-08 23:00:31 +0100

    Mutations and Actions Part 1 Completed

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from "@/services/EventService.js"
 
 Vue.use(Vuex)
 
@@ -7,22 +8,28 @@ export default new Vuex.Store({
   state: {
     user: { id: "abc123", name: "Adam Jahr"},
     categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'],
-    events: [
-      {id: 1, title: "...", organizer: "..."},
-      {id:2, title: "...", organizer: "..."},
-      {id:3, title: "...", organizer: "..."},
-      {id:4, title: "...", organizer: "..."},
-    ]
+    events: []
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event){
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({commit}, event){
+      return EventService.postEvent(event).then(() => {
+      commit("ADD_EVENT", event)
+    })
+//when our EventService responds, then we'll commit the add event mutation
+    }
+  },
   getters: {
-    catLength: state => state.categories.length,
-    doneTodos: state => state.todos.filter(todo => todo.done),
-    activeTodosCount1:(state, getters) =>{ //example of passing an entire getters  into another getters - wow - crazy!
-      return state.todos.length - getters.doneTodos.length
-    },
-    activeTodosCount: state => state.todos.filter(todo => !todo.done).length, //returning the diff. between total todos and todos that are done
+    //catLength: state => state.categories.length,
+    //doneTodos: state => state.todos.filter(todo => todo.done),
+    //activeTodosCount1:(state, getters) =>{ //example of passing an entire getters  into another getters - wow - crazy!
+    //  return state.todos.length - getters.doneTodos.length
+    //},
+    //activeTodosCount: state => state.todos.filter(todo => !todo.done).length, //returning the diff. between total todos and todos that are done
     getEventById: state => id => {
       return state.events.find(event => event.id === id)
     }
---
+++ src/views/EventCreateLearning.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+<h1>Create Event, {{ user.name}}</h1>
+<p>This event was created by {{ user.id}}</p>
+<p>There are {{catLength}} categories</p>
+<p>{{getEventById(2)}}</p>
+<ul>
+  <li v-for="cat in categories" :key="cat">{{cat}}</li>
+</ul>
+
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex"; //maps the state into computer properties
+  export default {
+    computed:{
+      ...mapGetters(["getEventById"]),
+      /*
+      getEvent(){
+        return this.$store.getters.getEventById
+      },
+      */
+      catLength(){
+        return this.$store.getters.catLength
+      },
+      ...mapState(["user", "categories"])
+  }
+  }
+
+     /*
+mapState({
+        user: "user",
+        categories: "categories",
+      })
+  
+
+      mapState({
+        userName: state => state.user.name,
+        userID: state => state.user.id,
+        categories: state => state.categories
+      })
+      userName(){
+        return this.$store.state.user.name
+      },
+      userID(){
+        return this.$store.state.user.id
+      }
+      */
+</script>
+
+<style scoped>
+
+</style>
